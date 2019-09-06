dbdb72cefed7e01eadecd8ee0c8834dcb0d3b7a3
guying <guying@kuaishou.com>
2019-09-06 20:24:08 +0800

    第一次上传

---
+++ 内置模块/Exception.py
@@ -0,0 +1,73 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Created on 2017-9-30
+
+@author: yingrinsing
+@github: git@github.com:yingrinsing/python_grammar.git
+@copyright: Apache License, Version 2.0
+"""
+
+
+import laok
+#===============================================================================
+# 
+#===============================================================================
+#ArithmeticError[<type 'type'>] [<type 'exceptions.ArithmeticError'>]
+    #AssertionError[<type 'type'>] [<type 'exceptions.AssertionError'>]
+    #AttributeError[<type 'type'>] [<type 'exceptions.AttributeError'>]
+    #BaseException[<type 'type'>] [<type 'exceptions.BaseException'>]
+    #BufferError[<type 'type'>] [<type 'exceptions.BufferError'>]
+    #BytesWarning[<type 'type'>] [<type 'exceptions.BytesWarning'>]
+    #DeprecationWarning[<type 'type'>] [<type 'exceptions.DeprecationWarning'>]
+    #EOFError[<type 'type'>] [<type 'exceptions.EOFError'>]
+    #EnvironmentError[<type 'type'>] [<type 'exceptions.EnvironmentError'>]
+    #Exception[<type 'type'>] [<type 'exceptions.Exception'>]
+    #FloatingPointError[<type 'type'>] [<type 'exceptions.FloatingPointError'>]
+    #FutureWarning[<type 'type'>] [<type 'exceptions.FutureWarning'>]
+    #GeneratorExit[<type 'type'>] [<type 'exceptions.GeneratorExit'>]
+    #IOError[<type 'type'>] [<type 'exceptions.IOError'>]
+    #ImportError[<type 'type'>] [<type 'exceptions.ImportError'>]
+    #ImportWarning[<type 'type'>] [<type 'exceptions.ImportWarning'>]
+    #IndentationError[<type 'type'>] [<type 'exceptions.IndentationError'>]
+    #IndexError[<type 'type'>] [<type 'exceptions.IndexError'>]
+    #KeyError[<type 'type'>] [<type 'exceptions.KeyError'>]
+    #KeyboardInterrupt[<type 'type'>] [<type 'exceptions.KeyboardInterrupt'>]
+    #LookupError[<type 'type'>] [<type 'exceptions.LookupError'>]
+    #MemoryError[<type 'type'>] [<type 'exceptions.MemoryError'>]
+    #NameError[<type 'type'>] [<type 'exceptions.NameError'>]
+    #NotImplementedError[<type 'type'>] [<type 'exceptions.NotImplementedError'>]
+    #OSError[<type 'type'>] [<type 'exceptions.OSError'>]
+    #OverflowError[<type 'type'>] [<type 'exceptions.OverflowError'>]
+    #PendingDeprecationWarning[<type 'type'>] [<type 'exceptions.PendingDeprecationWarning'>]
+    #ReferenceError[<type 'type'>] [<type 'exceptions.ReferenceError'>]
+    #RuntimeError[<type 'type'>] [<type 'exceptions.RuntimeError'>]
+    #RuntimeWarning[<type 'type'>] [<type 'exceptions.RuntimeWarning'>]
+    #StandardError[<type 'type'>] [<type 'exceptions.StandardError'>]
+    #StopIteration[<type 'type'>] [<type 'exceptions.StopIteration'>]
+    #SyntaxError[<type 'type'>] [<type 'exceptions.SyntaxError'>]
+    #SyntaxWarning[<type 'type'>] [<type 'exceptions.SyntaxWarning'>]
+    #SystemError[<type 'type'>] [<type 'exceptions.SystemError'>]
+    #SystemExit[<type 'type'>] [<type 'exceptions.SystemExit'>]
+    #TabError[<type 'type'>] [<type 'exceptions.TabError'>]
+    #TypeError[<type 'type'>] [<type 'exceptions.TypeError'>]
+    #UnboundLocalError[<type 'type'>] [<type 'exceptions.UnboundLocalError'>]
+    #UnicodeDecodeError[<type 'type'>] [<type 'exceptions.UnicodeDecodeError'>]
+    #UnicodeEncodeError[<type 'type'>] [<type 'exceptions.UnicodeEncodeError'>]
+    #UnicodeError[<type 'type'>] [<type 'exceptions.UnicodeError'>]
+    #UnicodeTranslateError[<type 'type'>] [<type 'exceptions.UnicodeTranslateError'>]
+    #UnicodeWarning[<type 'type'>] [<type 'exceptions.UnicodeWarning'>]
+    #UserWarning[<type 'type'>] [<type 'exceptions.UserWarning'>]
+    #ValueError[<type 'type'>] [<type 'exceptions.ValueError'>]
+    #Warning[<type 'type'>] [<type 'exceptions.Warning'>]
+    #WindowsError[<type 'type'>] [<type 'exceptions.WindowsError'>]
+    #ZeroDivisionError[<type 'type'>] [<type 'exceptions.ZeroDivisionError'>]
+def _lktest():
+    obj = None
+    laok.dump_description_help(obj)
+    
+
+
+if __name__ == '__main__':
+    laok.lktest_run()#catch_except = True
